267e33af347ab3645553cccfddda801b9b7e9e29
manavnagpal08 <72203264+manavnagpal08@users.noreply.github.com>
2025-11-30 23:22:43 +0530

    Update firebase.py

--- firebase.py
+++ firebase.py
@@ -1,24 +1,27 @@
 import requests
+import json
 
-FIREBASE_URL = "https://omss-2ccc6-default-rtdb.firebaseio.com/"
+BASE_URL = "https://omss-2ccc6-default-rtdb.firebaseio.com/"
 
 def read(path):
-    url = f"{FIREBASE_URL}{path}.json"
-    res = requests.get(url)
-    return res.json()
-
-def save(path, data):
-    url = f"{FIREBASE_URL}{path}.json"
-    res = requests.put(url, json=data)
-    return res.json()
-
-def update(path, data):
-    url = f"{FIREBASE_URL}{path}.json"
-    res = requests.patch(url, json=data)
-    return res.json()
+    url = f"{BASE_URL}{path}.json"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    return None
 
 def push(path, data):
-    url = f"{FIREBASE_URL}{path}.json"
-    res = requests.post(url, json=data)
-    return res.json()
+    url = f"{BASE_URL}{path}.json"
+    response = requests.post(url, json=data)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Exception(f"Firebase Push Failed: {response.text}")
 
+def update(path, data):
+    url = f"{BASE_URL}{path}.json"
+    response = requests.patch(url, json=data)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Exception(f"Firebase Update Failed: {response.text}")
