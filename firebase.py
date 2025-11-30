ff8bda4cc43a4f5d9e7cda5c7ef10bc9e04c204a
manavnagpal08 <72203264+manavnagpal08@users.noreply.github.com>
2025-11-30 22:51:45 +0530

    Update firebase.py

--- firebase.py
+++ firebase.py
@@ -1,1 +1,24 @@
+import requests
+
+FIREBASE_URL = "https://omss-2ccc6-default-rtdb.firebaseio.com/"
+
+def read(path):
+    url = f"{FIREBASE_URL}{path}.json"
+    res = requests.get(url)
+    return res.json()
+
+def save(path, data):
+    url = f"{FIREBASE_URL}{path}.json"
+    res = requests.put(url, json=data)
+    return res.json()
+
+def update(path, data):
+    url = f"{FIREBASE_URL}{path}.json"
+    res = requests.patch(url, json=data)
+    return res.json()
+
+def push(path, data):
+    url = f"{FIREBASE_URL}{path}.json"
+    res = requests.post(url, json=data)
+    return res.json()
 
