d78d70e0ac1f61aa1316c0ebf6d3c94e2612846f
manavnagpal08 <72203264+manavnagpal08@users.noreply.github.com>
2025-12-01 10:42:37 +0530

    Update login.py

--- login.py
+++ login.py
@@ -23,7 +23,7 @@ st.title("🔐 Login to OMS")
 # HARDCODED USER DATABASE
 # ---------------------------
 USERS = {
-    "admin6": {"password": "admin123", "role": "admin"},
+    "admin7": {"password": "admin123", "role": "admin"},
     "design01": {"password": "design123", "role": "design"},
     "print04": {"password": "print123", "role": "printing"},
     "die04": {"password": "die123", "role": "diecut"},
